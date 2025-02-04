40016d8910761e82fbb7597e76562ec1c1bfa977
Emma <Emma@fanap.ir>
2025-02-04 18:36:48 +0330

    add fonts and tailwind config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,3 @@
-// import js from '@eslint/js';
-// import reactHooks from 'eslint-plugin-react-hooks';
-// import reactRefresh from 'eslint-plugin-react-refresh';
-// import globals from 'globals';
-// import tseslint from 'typescript-eslint';
-
-// export default tseslint.config(
-//   { ignores: ['dist'] },
-//   {
-//     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-//     files: ['**/*.{ts,tsx}'],
-//     languageOptions: {
-//       ecmaVersion: 2020,
-//       globals: globals.browser,
-//     },
-//     plugins: {
-//       'react-hooks': reactHooks,
-//       'react-refresh': reactRefresh,
-//     },
-//     rules: {
-//       ...reactHooks.configs.recommended.rules,
-//       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-//     },
-//   },
-// );
 import { init } from '@fullstacksjs/eslint-config';
 
 export default [
@@ -35,6 +10,7 @@ export default [
     react: true,
     strict: true,
     prettier: true,
+    tailwind: true,
   }),
   {
     // Global ignores
