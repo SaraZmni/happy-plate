c8108d4be2bf62c22be9c0c8e914e15276a4240a
SaraZmni <szamani240@gmail.com>
2025-03-26 12:03:11 -0700

    start ordering food component

--- src/routes/meal-booking/meal-booking.tsx
+++ src/routes/meal-booking/meal-booking.tsx
@@ -1,7 +1,31 @@
 import type { FC } from 'react';
 
 const MealBooking: FC = () => {
-  return <div>MealBooking</div>;
+  return (
+    <>
+      <div className="grid grid-cols-[1fr_2fr_2fr_2fr] place-items-center gap-4 p-4">
+        <div className="col-start-2">صبحانه</div>
+        <div>ناهار</div>
+        <div>شام</div>
+      </div>
+      <div className="mx-auto grid w-11/12 grid-cols-[1fr_2fr_2fr_2fr] place-items-center border-2">
+        <time>
+          <div className="font-medium">شنبه</div>
+          <div className="text-sm text-gray-500">1402/5/23</div>
+        </time>
+        <div>
+          <label class="flex items-center gap-3 text-lg font-semibold">
+            <input className="rounded border-gray-400" name="food" type="radio" value="نان پنیر" />
+            نان پنیر
+          </label>
+          <label class="flex items-center gap-3 text-lg font-semibold">
+            <input className="rounded border-gray-400" name="food" type="radio" value="عدسی" />
+            عدسی
+          </label>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MealBooking;
