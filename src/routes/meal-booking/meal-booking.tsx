b1981847897b5c3d4d120e7abe2d1a5741abc1d5
SaraZmni <szamani240@gmail.com>
2025-03-26 14:17:31 -0700

    start ordering food component

--- src/routes/meal-booking/meal-booking.tsx
+++ src/routes/meal-booking/meal-booking.tsx
@@ -1,6 +1,13 @@
 import type { FC } from 'react';
 
+import { useState } from 'react';
+
 const MealBooking: FC = () => {
+  const [selectedKey, setSelectedKey] = useState('');
+
+  const handleSelection = (key: string) => {
+    setSelectedKey(key);
+  };
   return (
     <>
       <div className="grid grid-cols-[1fr_2fr_2fr_2fr] place-items-center gap-4 p-4">
@@ -8,18 +15,40 @@ const MealBooking: FC = () => {
         <div>ناهار</div>
         <div>شام</div>
       </div>
-      <div className="mx-auto grid w-11/12 grid-cols-[1fr_2fr_2fr_2fr] place-items-center border-2">
+      <div className="border-layout-main mx-auto grid w-11/12 grid-cols-[1fr_2fr_2fr_2fr] place-items-center py-2 shadow-md">
         <time>
           <div className="font-medium">شنبه</div>
           <div className="text-sm text-gray-500">1402/5/23</div>
         </time>
-        <div>
-          <label class="flex items-center gap-3 text-lg font-semibold">
-            <input className="rounded border-gray-400" name="food" type="radio" value="نان پنیر" />
+
+        <div className="w-4/5 bg-white">
+          <label
+            className={`border-layout-main flex items-center gap-3  py-1 text-sm font-semibold transition ${
+              selectedKey === 'key1' ? 'bg-green-200 text-green-700' : ''
+            }`}
+          >
+            <input
+              className="mx-2 rounded border-gray-400"
+              data-key="key1"
+              name="food"
+              type="radio"
+              onChange={(e) => handleSelection(e.target.dataset.key ?? '')}
+            />
             نان پنیر
           </label>
-          <label class="flex items-center gap-3 text-lg font-semibold">
-            <input className="rounded border-gray-400" name="food" type="radio" value="عدسی" />
+
+          <label
+            className={`border-layout-main flex items-center gap-3  py-1 text-sm font-semibold transition ${
+              selectedKey === 'key2' ? 'bg-green-200 text-green-700' : ''
+            }`}
+          >
+            <input
+              className="mx-2 rounded border-gray-400"
+              data-key="key2"
+              name="food"
+              type="radio"
+              onChange={(e) => handleSelection(e.target.dataset.key ?? '')}
+            />
             عدسی
           </label>
         </div>
